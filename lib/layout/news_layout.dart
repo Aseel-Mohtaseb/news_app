8b4bc79f6192c7cc56628f33c9bf74ac7633548e
Aseel-Mohtaseb <aseelamermohtaseb@gmail.com>
2023-06-24 15:41:45 +0300

    first commit

---
+++ lib/layout/news_layout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class NewsLayout extends StatelessWidget {
+  const NewsLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('News App');
+  }
+}
